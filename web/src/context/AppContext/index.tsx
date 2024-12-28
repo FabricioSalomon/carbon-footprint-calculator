93c7729adbf08e48f9c653876b41fffae035c7ae
Fabricio Salomon <fabriciosalomon14@gmail.com>
2024-12-28 11:56:18 -0300

    feat: add app context

--- web/src/context/AppContext/index.tsx
+++ web/src/context/AppContext/index.tsx
@@ -1,6 +1,10 @@
+import { darkTheme } from "@/theme";
+import { ThemeEnum } from "@/types";
 import { ReactNode, createContext, useContext } from "react";
+import { useTheme } from "styled-components";
+import { Context, States } from "./types";
 
-interface AppContextProps {}
+interface AppContextProps extends Context {}
 
 interface AppProviderProps {
   children: ReactNode;
@@ -9,11 +13,13 @@ interface AppProviderProps {
 const AppContext = createContext<AppContextProps>({} as AppContextProps);
 
 export function AppProvider({ children }: Readonly<AppProviderProps>) {
-  const states = {
-    test: "",
+  const theme = useTheme();
+
+  const states: States = {
+    theme: theme === darkTheme ? ThemeEnum.DARK : ThemeEnum.LIGHT,
   };
 
-  const context = {
+  const context: Context = {
     ...states,
   };
 
---
+++ web/src/context/AppContext/types.ts
@@ -0,0 +1,9 @@
+import { ThemeEnum } from "@/types";
+
+export type States = {
+  theme: ThemeEnum;
+};
+
+export type Methods = {};
+
+export type Context = States & Methods;
